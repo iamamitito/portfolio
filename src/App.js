51757d53e31faeea28b5bb200471d2e741fa881e
Héctor Espinoza <hespinoza@uchile.cl>
2020-07-19 17:54:57 -0400

    style changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import FullProject from './components/FullProject/FullProject';
 import Projects from './components/Projects/Projects';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './bootstrap-grid.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <h3><i className="fas fa-code fa-2x"></i> Héctor Espinoza</h3>
         <Switch>
           <Route path="/" exact component={Projects} />
           <Route path={'/:name/:id'} exact component={FullProject} />
--- src/components/FullProject/FullProject.js
+++ src/components/FullProject/FullProject.js
@@ -7,7 +7,6 @@ const fullProject = (props) => {
     let fullProject = data.map(project =>
         props.match.params.id === project.id ? (
             <div className="FullProject" key={project.id}>
-                <p>{project.name}</p>
                 <img src={project.url} alt={project.name} />
                 <p>{project.description}</p>
                 <p><a href={project.projectUrl}>ver proyecto</a></p>
@@ -15,10 +14,9 @@ const fullProject = (props) => {
         ) : null
     );
     return (
-        <div className="FullProject container">
+        <div className="FullProject">
             <h1>{props.match.params.name}</h1>
             {fullProject}
-            <p>Github link</p>
         </div>
     );
 }
